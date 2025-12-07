617a13ec889e00e79aaa087dfdd6408fa51b1ca0
NothRen <antoine.oriol@protonmail.com>
2025-12-07 16:43:52 +0100

    Edit docker files and scripts to work with UV

--- start.sh
+++ start.sh
@@ -3,20 +3,20 @@ set -e
 
 #curl -sSL https://install.python-poetry.org | python3
 export PATH="/home/fossbadge/.local/bin:$PATH"
-poetry install
-echo "Poetry install ok"
+uv sync
+echo "UV install ok"
 
-poetry run python3 manage.py migrate
+uv run python3 manage.py migrate
 
 # Install if no asset created :
-poetry run python3 manage.py install
+uv run python3 manage.py install
 # New static for nginx ?
-poetry run python3 manage.py collectstatic --noinput
+uv run python3 manage.py collectstatic --noinput
 
 echo "Run GUNICORN"
 echo "You should be able to see the Fedow dashbord at :"
 echo "https://$DOMAIN/dashboard/"
 sqlite3 ./database/db.sqlite3 'PRAGMA journal_mode=WAL;'
 sqlite3 ./database/db.sqlite3 'PRAGMA synchronous=normal;'
-poetry run gunicorn fossbadge.wsgi --log-level=info --log-file /home/fossbadge/FossBadge/logs/gunicorn.logs -w 3 -b 0.0.0.0:8000
+uv run gunicorn fossbadge.wsgi --log-level=info --log-file /home/fossbadge/FossBadge/logs/gunicorn.logs -w 3 -b 0.0.0.0:8000
 

f60278b2544f458f466b9ffdbfda94af8d1d517a
NothRen <antoine.oriol@protonmail.com>
2026-03-05 10:03:46 +0100

    Add new options to popup

--- core/templatetags/custom_tags.py
+++ core/templatetags/custom_tags.py
@@ -8,14 +8,21 @@ POPUP_OPEN_JS_FUNCTION = "openPopup"
 POPUP_CONTENT_ID = "customPopup-content"
 
 @register.simple_block_tag
-def popup(content, popup_width="40%"):
+def popup(content, popup_width="40%", *args, **kwargs):
+    outside_click_close = kwargs.pop("outside_click_close", False)
+    esc_key_close = kwargs.pop("esc_key_close", True)
+
+
     template = loader.get_template('includes/popup.html')
+
     return template.render({
         "content": content,
         "open_func_name": POPUP_OPEN_JS_FUNCTION,
         "close_func_name": POPUP_CLOSE_JS_FUNCTION,
         "popup_content_id":POPUP_CONTENT_ID,
-        "popup_width":popup_width
+        "popup_width":popup_width,
+        "outside_click_close":outside_click_close,
+        "esc_key_close" : esc_key_close
     })
 
 @register.simple_tag
